f0d4de58b240c8ad038f6387bf9ff04ea68e899c
PJSYSTEM789 <psingtoroon@gmail.com>
2026-09-16 06:36:26 +0700

    Update index.php

--- app/index.php
+++ app/index.php
@@ -40,7 +40,15 @@
     
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1BXN2PF65H"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-1BXN2PF65H');
+</script>
     <!-- Custom CSS Styles -->
     <style>
         body {
